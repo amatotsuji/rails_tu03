9be497cba696e48540155ee90ee939f0db48f4c5
amato.tsuji <kabanokakuni@gmail.com>
2015-03-24 19:57:56 +0900

    5.3章リファクタリング

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,59 +2,36 @@ require 'spec_helper'
 
 describe "Static pages" do
 	
+	subject { page }
+	
 	describe "Home page" do
+		before{visit root_path}
 		
-		it "should have the 'Sample App'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
-		end
-		
-		it "should have the right title 'Home'" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Home")
-		end
+		it { should have_content('Sample App') }
 		
 	end
 	
 	describe "Help page" do
+		before{visit help_path}
 		
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
-		
-		it "should have the right title 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_title('Help')
-		end
+		it { should have_content('Help') }
+		it { should have_title('Help') }
 		
 	end
 	
 	describe "About page" do
-	
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
+		before{visit about_path}
 		
-		it "should have the right title 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_title('About Us')
-		end
+		it { should have_content('About') }
+		it { should have_title('About') }
 	
 	end
 	
 	describe "Contact page" do
+		before{visit contact_path}
 		
-		it "should have the content 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_content('Contact')
-		end
-		
-		it "should have the right title 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_title('Contact')
-		end
+		it { should have_content('Contact') }
+		it { should have_title('Contact') }
 		
 	end
 	
