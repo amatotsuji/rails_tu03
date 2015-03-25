833197b39d58135841bf47c56238bbc6fc94068f
amato.tsuji <kabanokakuni@gmail.com>
2015-03-25 12:51:13 +0900

    5章終了

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,34 +4,63 @@ describe "Static pages" do
 	
 	subject { page }
 	
+	shared_examples_for "all static pages" do
+	
+		it { should have_content(heading) }
+		it { should have_title(full_title(page_title)) }
+	
+	end
+	
 	describe "Home page" do
 		before{visit root_path}
+		let(:heading) {'Sample App'}
+		let(:page_title) {''}
 		
-		it { should have_content('Sample App') }
+		it_should_behave_like("all static pages")
+		it { should_not have_title('| Home') }
 		
 	end
 	
 	describe "Help page" do
-		before{visit help_path}
+		before{visit help_path}		
+		let(:heading) {'Help'}
+		let(:page_title) {'Help'}
 		
-		it { should have_content('Help') }
-		it { should have_title('Help') }
+		it_should_behave_like("all static pages")
 		
 	end
 	
 	describe "About page" do
 		before{visit about_path}
+		let(:heading) {'About'}
+		let(:page_title) {'About'}
 		
-		it { should have_content('About') }
-		it { should have_title('About') }
+		it_should_behave_like("all static pages")
 	
 	end
 	
 	describe "Contact page" do
-		before{visit contact_path}
+		before{visit contact_path}				
+		let(:heading) {'Contact'}
+		let(:page_title) {'Contact'}
 		
-		it { should have_content('Contact') }
-		it { should have_title('Contact') }
+		it_should_behave_like("all static pages")
+		
+	end
+	
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title(full_title('About'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to have_title(full_title('Contact'))
+		click_link "Home"
+		click_link "Sign up now."
+		expect(page).to have_title(full_title('Sign up'))
+		click_link "sample app"
+		expect(page).to have_title(full_title(''))
 		
 	end
 	
