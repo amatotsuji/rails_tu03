b93a6dd19fa77fe121ddadab90c6be87ed67d7f1
amato.tsuji <kabanokakuni@gmail.com>
2015-03-26 19:35:39 +0900

    8.2章まで終了
    ユーザーのサインイン、アウトの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
 	it{ should respond_to(:password) }
 	it{ should respond_to(:password_confirmation) }
 	it{ should respond_to(:authenticate) }
+	it{ should respond_to(:remember_token) }
 	
 	
 	it{ should be_valid }
@@ -103,6 +104,11 @@ describe User do
 			specify{ expect(user_for_invalid_password).to be_false }
 		end
 		
+	end
+	
+	describe "remember token" do
+		before{ @user.save }
+		its(:remember_token){ should_not be_blank }
 	end
 	
 	
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,6 +52,7 @@ describe "UserPages" do
 					before{ click_button submit }
 					let(:user){ User.find_by(email: "user@example.com") }
 					
+					it{ should have_link('Sign out') }
 					it{ should have_title(user.name) }
 					it{ should have_selector('div.alert.alert-success', text: 'Welcome') }
 				end
