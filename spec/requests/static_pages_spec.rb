db665c789ae2853c8219807ac2499701ab23478c
amato.tsuji <kabanokakuni@gmail.com>
2015-04-01 17:39:52 +0900

    11.2章ajax対応のフォローボタンの実装まで

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,6 +40,19 @@ describe "Static pages" do
 			it "should render the paginate of user's micropost" do
 				expect(page).to have_selector(".micropost_paginate")
 			end
+			
+			describe "follower/following counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+				
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 followers", href: followers_user_path(user)) }
+				
+			end
+			
 		end
 		
 	end
